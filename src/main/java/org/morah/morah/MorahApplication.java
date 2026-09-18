770394ff44be65bcc10ecd5d64785563f01ed3cd
BernardoPr <bernardoplottegher@gmail.com>
2026-09-18 20:44:09 -0300

    Main

---
+++ src/main/java/org/morah/morah/MorahApplication.java
@@ -0,0 +1,13 @@
+package org.morah.morah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorahApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/morah/morah/MorahApplicationTests.java
@@ -0,0 +1,13 @@
+package org.morah.morah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
